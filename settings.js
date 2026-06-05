c421ba19b73df3da98692e38cdaba22f6668e7c2
vixzz-dev <vixzzdeveloper@gmail.com>
2026-06-06 02:01:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  PORT: 1320,
-  DOMAIN: "http://private02.angkasanyabobo.my.id",
+  PORT: 2556,
+  DOMAIN: "bandar-panel.astralmarket.my.id",
   OWNER_ID: 7846858206,
   TOKEN: "8402377706:AAHnsJKnho6R1T5cYeJ4fWAyLtRDtwmn0hk"
 };
